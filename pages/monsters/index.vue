f4a114bb7eeb88c0c1ce3493ddb6c0679c30e34b
sturlen <43607012+Sturlen@users.noreply.github.com>
2024-08-04 17:15:36 +0200

    V2 monster list first pass

--- pages/monsters/index.vue
+++ pages/monsters/index.vue
@@ -29,7 +29,9 @@
         v-model="debouncedFilter"
         endpoint="monsters"
         :api-endpoint="API_ENDPOINTS.monsters"
-        :cols="['type', 'cr', 'size', 'hit_points']"
+        :cols="['type', 'challenge_rating_decimal', 'size', 'hit_points']"
+        :fields="['key', 'challenge_rating_text']"
+        :transform="transform"
       />
     </div>
   </section>
@@ -39,6 +41,21 @@
 import ApiResultsTable from '~/components/ApiResultsTable.vue';
 import FilterButton from '~/components/FilterButton.vue';
 import MonsterFilterBox from '~/components/MonsterFilterBox.vue';
+
+function transform(creature) {
+  console.log(
+    'Transforming creature',
+    creature,
+    creature.challenge_rating_text
+  );
+  return {
+    ...creature,
+    slug: creature.key,
+    type: new URL(creature.type).pathname.split('/').at(-2),
+    size: creature.size.name,
+    challenge_rating_decimal: creature.challenge_rating_text,
+  };
+}
 
 const displayFilter = ref(false);
 
